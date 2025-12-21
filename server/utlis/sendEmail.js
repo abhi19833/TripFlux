1d5595cf7c5c544c40a61a5f87d504ceaaccf52e
abhi19833 <abhi189012@gamil.com>
2025-12-21 21:56:54 +0530

    Fix Mailtrap nodemailer config

--- server/utlis/sendEmail.js
+++ server/utlis/sendEmail.js
@@ -2,11 +2,13 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: process.env.MAILTRAP_HOST,
-  port: process.env.MAILTRAP_PORT,
+  port: Number(process.env.MAILTRAP_PORT),
+  secure: false,
   auth: {
     user: process.env.MAILTRAP_USER,
     pass: process.env.MAILTRAP_PASS,
   },
+  connectionTimeout: 20000,
 });
 
 module.exports = transporter;
