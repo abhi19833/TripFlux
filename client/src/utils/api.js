e9eaae2ad6c5535f06c57429447d8d47209ee235
abhi19833 <abhi189012@gamil.com>
2025-12-25 10:40:39 +0530

    l

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://tripflux-j04l.onrender.com/api",
+  baseURL: "https://tripflux-j04l.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
